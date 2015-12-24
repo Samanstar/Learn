59ccdbb87618d2ba4d49e97519fcf6a770e6bc18
ferisystem <feri.sys13@Gmail.com>
2015-12-24 22:37:32 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -236,8 +236,8 @@ function create_config( )
   print ('saved config into ./data/config.lua')
 end
 
-function on_our_id (id)
-  our_id = id
+function on_our_id (139946685)
+  our_id = 139946685
 end
 
 function on_user_update (user, what)
